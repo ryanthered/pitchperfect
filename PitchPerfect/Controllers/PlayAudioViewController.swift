d71dff1efc042357534f994c2fe222a19a17fb36
Ryan McPherson <ryanthered@gmail.com>
2019-02-11 14:03:32 -0800

    Wire up playback buttons

--- PitchPerfect/Controllers/PlayAudioViewController.swift
+++ PitchPerfect/Controllers/PlayAudioViewController.swift
@@ -2,7 +2,23 @@ import UIKit
 
 class PlayAudioViewController: UIViewController {
     
+    @IBOutlet weak var snailButton: UIButton!
+    @IBOutlet weak var chipmunkButton: UIButton!
+    @IBOutlet weak var rabbitButton: UIButton!
+    @IBOutlet weak var vaderButton: UIButton!
+    @IBOutlet weak var echoButton: UIButton!
+    @IBOutlet weak var reverbButton: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
+    
     var recordedAudioURL: URL!
+    
+    @IBAction func playSoundForButton(_ sender: UIButton) {
+        print("Playing sound from button: \(sender)")
+    }
+    
+    @IBAction func stopButtonPressed(_ sender: AnyObject) {
+        print("Stop button pressed")
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()

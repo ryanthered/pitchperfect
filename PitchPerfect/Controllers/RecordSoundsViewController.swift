ec55bf32b157c5e960ae61a6170c48bb2668531b
Ryan McPherson <ryanthered@gmail.com>
2019-02-11 15:21:57 -0800

    Add audio extension and wire up action methods

--- PitchPerfect/Controllers/RecordSoundsViewController.swift
+++ PitchPerfect/Controllers/RecordSoundsViewController.swift
@@ -1,7 +1,7 @@
 import AVFoundation
 import UIKit
 
-class RecordAudioViewController: UIViewController, AVAudioRecorderDelegate {
+class RecordSoundsViewController: UIViewController, AVAudioRecorderDelegate {
     
     var audioRecorder: AVAudioRecorder!
 
@@ -59,7 +59,7 @@ class RecordAudioViewController: UIViewController, AVAudioRecorderDelegate {
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "stopRecording" {
-            let playSoundsVC = segue.destination as! PlayAudioViewController
+            let playSoundsVC = segue.destination as! PlaySoundsViewController
             let recordedAudioURL = sender as! URL
             playSoundsVC.recordedAudioURL = recordedAudioURL
         }
